9b0d4c0525e4f1db1476c8f6093c87196c984d3b
Didier Parat <didier.parat@telenordigital.com>
2018-03-12 22:32:07 +0100

    Create structure.

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class HelloWorld extends Component {
+    render() {
+        return (
+            <div className="title">
+                <h1>API Visual</h1>
+            </div>
+        );
+    }
+}
+
+export default HelloWorld;
